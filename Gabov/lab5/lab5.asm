25ad1c891d5a294352a2e177c75959ac6758e5ae
egor <gsa74@yandex.ru>
2019-06-10 01:33:59 +0300

    finish

--- Gabov/lab5/lab5.asm
+++ Gabov/lab5/lab5.asm
@@ -7,10 +7,9 @@ CODE SEGMENT
 START: JMP MAIN
 
 DATA SEGMENT
-	str_loaded DB 'New interruption is succesfully loaded!',0DH,0AH,'$'
-	str_already_loaded DB 'New interruption has been already loaded!',0DH,0AH,'$'
-	str_unloaded DB 'New interruption is unloaded!',0DH,0AH,'$'
-	endl db 0DH,0AH,'$'
+	str_loaded DB 'Interruption is  loaded!',0DH,0AH,'$'
+	str_already_loaded DB 'Interruption was already loaded!',0DH,0AH,'$'
+	str_unloaded DB 'Interruption is unloaded!',0DH,0AH,'$'
 DATA ENDS
 
 STACK SEGMENT STACK
@@ -21,10 +20,10 @@ STACK ENDS
 ROUT proc far
 	jmp ROUT_begin
 ROUT_DATA:
-	SIGNATURE DB 'SIGN' ;сигнатура, некоторый код, который идентифицирует резидент
-	KEEP_IP DW 0 ;и смещения прерывания
-	KEEP_CS DW 0 ;для хранения сегмента
-	KEEP_PSP DW 0 ;и PSP
+	FLAG DB 'ZZZZ' ; дает понять, что это нужный обработчик
+	KEEP_IP DW 0 
+	KEEP_CS DW 0 
+	KEEP_PSP DW 0 
 	KEEP_SS DW 0
 	KEEP_AX DW 0	
 	KEEP_SP DW 0 
@@ -44,7 +43,6 @@ ROUT_begin:
 	
 	;проверяем скан-код far
 	in al, 60H ;читать ключ
-	;cmp al, 0Eh ;это требуемый код? 0E - скан-код Backspace
 	cmp al, 02h; 1
 	je DO_REQ ;получили требуемый скан-код
 	
@@ -56,9 +54,9 @@ ROUT_begin:
 ; собственный обработчик
 DO_REQ:
 	push ax
-	in al, 61h ;взять значение порта управления клавиатурой
-	mov ah, al ;сохранить его
-	or al, 80h ;установить бит разрешения для клавиатуры
+	in al, 61h  ;взять значение порта управления клавиатурой
+	mov ah, al  ;сохранить его
+	or al, 80h  ;установить бит разрешения для клавиатуры
 	out 61h, al ;и вывести его в управляющий порт
 	xchg ah, al ;извлечь исходное значение порта
 	out 61h, al ;и записать его обратно
@@ -88,53 +86,55 @@ ROUT_END:
 	mov ss, KEEP_SS
 	mov sp, KEEP_SP
 	mov ax, KEEP_AX
-	mov al,20h
-	out 20h,al
+	mov al, 20h
+	out 20h, al
 	iret
 LAST_BYTE:
 ROUT ENDP
 	
-; Сокращение для функции вывода.
 PRINT_DX proc near
 	mov ah,09h
 	int 21h
 	ret
 PRINT_DX endp
 
-; Проверка состояния загрузки нового прерывания в память
+
 CHECK_HANDLER proc near
-	mov ah,35h 
-	mov al,09h 
-	int 21h ; в es bx получим адрес обработчика прерываний
-	mov si, offset SIGNATURE 
+
+	; получаем адрес обработчика по номеру
+	mov ah, 35h 
+	mov al, 09h 
+	int 21h ;
+ 
+	mov si, offset FLAG
 	sub si, offset ROUT ; в si смещение сигнатуры от начала функции
 	
-	mov ax,'IS'
-	cmp ax,es:[bx+si]
+	mov ax, 'ZZ'
+	cmp ax, es:[bx+si]
 	jne not_loaded
-	mov ax, 'NG'
-	cmp ax,es:[bx+si+2] 
+	mov ax, 'ZZ'
+	cmp ax, es:[bx+si+2] 
 	je loaded
+
 	; Загружаем новый Обработчик
 not_loaded:
 	call SET_HANDLER
-	; Вычисляем память для резидента
-	mov dx,offset LAST_BYTE ; в байтах
-	mov cl,4 ;в параграфы в dx
-	shr dx,cl
+	mov dx, offset LAST_BYTE ; в байтах
+	mov cl, 4 ;в параграфы в dx
+	shr dx, cl
 	inc dx
-	add dx,CODE ;прибавляем адрес code seg
-	sub dx,CS:KEEP_PSP ;вычитаем адрес psp
-	xor al,al
-	mov ah,31h
+	add dx, CODE ;прибавляем адрес code 
+	sub dx, CS:KEEP_PSP 
+	xor al, al
+	mov ah, 31h
 	int 21h 
 
-; Проверка аргумента cmd
+;проверка агрумента командной строки
 loaded: 
 	push es
 	push ax
-	mov ax,KEEP_PSP 
-	mov es,ax
+	mov ax, KEEP_PSP 
+	mov es, ax
 	cmp byte ptr es:[82h],'/' 
 	jne args_false
 	cmp byte ptr es:[83h],'u' 
@@ -145,7 +145,7 @@ loaded:
 args_false:
 	pop ax
 	pop es
-	mov dx,offset str_already_loaded
+	mov dx, offset str_already_loaded
 	call PRINT_DX
 	ret
 
@@ -164,21 +164,21 @@ SET_HANDLER proc near
 	push dx
 	push ds
 
-	mov ah,35h
-	mov al,09h
+	mov ah, 35h
+	mov al, 09h
 	int 21h; es:bx
 	mov CS:KEEP_IP,bx 
 	mov CS:KEEP_CS,es
 
-	mov dx,offset ROUT
-	mov ax,seg ROUT
-	mov ds,ax
-	mov ah,25h
-	mov al,09h
+	mov dx, offset ROUT
+	mov ax, seg ROUT
+	mov ds, ax
+	mov ah, 25h
+	mov al, 09h
 	int 21h
 
 	pop ds
-	mov dx,offset str_loaded
+	mov dx, offset str_loaded
 	call PRINT_DX
 	pop dx
 	ret
@@ -192,17 +192,17 @@ DELETE_HANDLER proc
 		mov ax,ES:[BX+SI+6] ; CS
 		mov ds,ax
 		
-		mov ax,2509h
+		mov ax, 2509h
 		int 21h 
 	; Освобождаем память:
 		push es
-		mov ax,ES:[BX+SI+8] ; PSP
-		mov es,ax 
-		mov es,es:[2Ch] ; Блока переменных среды
-		mov ah,49h         
+		mov ax, ES:[BX+SI+8] ; PSP
+		mov es, ax 
+		mov es, es:[2Ch] ; Блока переменных среды
+		mov ah, 49h         
 		int 21h
 		pop es
-		mov es,ES:[BX+SI+8] ; PSP ; Блока резидентной программы
+		mov es, ES:[BX+SI+8] ; PSP ; Блока резидентной программы
 		mov ah, 49h
 		int 21h	
 		STI
@@ -214,12 +214,12 @@ DELETE_HANDLER ENDP
 MAIN:
 	mov AX,DATA
 	mov DS,AX
-	mov KEEP_PSP,ES
+	mov KEEP_PSP, ES
 	
 	call CHECK_HANDLER
 
-	xor AL,AL
-	mov AH,4Ch
+	xor AL, AL
+	mov AH, 4Ch
 	int 21H
 	CODE ENDS	
 
